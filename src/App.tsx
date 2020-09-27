3d203edeb83f698f5ba154a23eab70da81d49eb0
OrangeX4 <318483724@qq.com>
2020-09-27 15:14:39 +0800

    Add selectImg

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import Item from './components/Item'
 function App() {
   return (
     <div className="App">
-      <Item name=".." description="Return to parent folder"></Item>
+      <Item name=".." description="Return to parent folder" icon='folder'></Item>
+      <Item name="." description="Refresh current folder" icon='folder'></Item>
     </div>
   );
 }
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,25 +1,39 @@
 import React from 'react'
 import '../css/Item.css'
 import folderImg from '../assets/folder.png'
+import fileImg from '../assets/file.png'
 
 interface Props {
-    name: String;
-    description: String
+    name: string;
+    description: string;
+    icon: string
 }
 
 function Item(props: Props) {
-return (
-    <div>
-        <div className="root">
-            <img src={folderImg} alt='folder' />
-            <div className="text">
-                <span className="name">{ props.name }</span>
-                <br />
-                <span className="description">{ props.description }</span>
+
+    function selectImg(name: string) {
+        switch (name) {
+            case 'folder':
+                return folderImg
+            case 'file':
+                return fileImg
+            default:
+                return fileImg
+        }
+    }
+
+    return (
+        <div>
+            <div className="root">
+                <img src={selectImg(props.icon)} alt={props.icon} />
+                <div className="text">
+                    <span className="name">{props.name}</span>
+                    <br />
+                    <span className="description">{props.description}</span>
+                </div>
             </div>
         </div>
-    </div>
-)
+    )
 }
 
 export default Item
