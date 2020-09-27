a0bca2e3c30b6483c9a3217a53df06021dc8590c
OrangeX4 <318483724@qq.com>
2020-09-27 17:21:35 +0800

    Add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Item from './components/Item'
 import Breadcrumb from './components/BreadCrumb'
@@ -6,13 +6,16 @@ import Breadcrumb from './components/BreadCrumb'
 import example from './test/example.json'
 
 function App() {
+  // eslint-disable-next-line
+  const [state, setState] = useState(example);
+
   return (
     <div className='App'>
-      <Breadcrumb text={example.current} />
+      <Breadcrumb text={state.current} />
       <Item name='..' description='Return to parent folder' icon='folder' />
       <Item name='.' description='Refresh current folder' icon='folder' />
-      {example.dirs.map((dir) => <Item name={dir.name} description={dir.items + ' Items'} icon='folder' key={dir.name} />)}
-      {example.files.map((file) => <Item name={file.name} description={file.showSize + ' | ' + file.lastTime} icon='file' key={file.name} />)}
+      {state.dirs.map((dir) => <Item name={dir.name} description={dir.items + ' Items'} icon='folder' key={dir.name} />)}
+      {state.files.map((file) => <Item name={file.name} description={file.showSize + ' | ' + file.lastTime} icon='file' key={file.name} />)}
     </div>
   );
 }
