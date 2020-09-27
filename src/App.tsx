1dcbf3d41a222758c8768ef4e8494e02ad213840
OrangeX4 <318483724@qq.com>
2020-09-27 16:43:38 +0800

    Add files and folders map function

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ function App() {
       <Breadcrumb text={example.current} />
       <Item name='..' description='Return to parent folder' icon='folder' />
       <Item name='.' description='Refresh current folder' icon='folder' />
+      {example.dirs.map((dir) => <Item name={dir.name} description={dir.items + ' Items'} icon='folder' key={dir.name} />)}
+      {example.files.map((file) => <Item name={file.name} description={file.showSize + ' | ' + file.lastTime} icon='file' key={file.name} />)}
     </div>
   );
 }
