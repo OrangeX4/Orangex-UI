e0eaaf39b95279334d3aa236db9a1306a60bc39b
OrangeX4 <318483724@qq.com>
2020-10-04 16:04:38 +0800

    Add LongPress Event

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -5,11 +5,13 @@ import fileImg from '../assets/file.png'
 import selectPng from '../assets/select.png'
 
 interface Props {
-    name: string;
-    description: string;
-    icon: string;
-    isSelect?: boolean;
-    onClick?: ((event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void) | undefined
+    name: string
+    description: string
+    icon: string
+    isSelect?: boolean
+    onClick?: () => void | undefined
+    onLongPress?: () => void | undefined
+
 }
 
 function Item(props: Props) {
@@ -25,8 +27,36 @@ function Item(props: Props) {
         }
     }
 
+    let touchTimeStamp: number
+    let isTouch: boolean = true
+
+    function handleTouchStart() {
+        isTouch = true
+        touchTimeStamp = new Date().getTime()
+    }
+
+    function handleTouchEnd() {
+        let currentTimeStamp = new Date().getTime()
+        if(isTouch){
+            if (currentTimeStamp - touchTimeStamp < 500) {
+                if (props.onClick) props.onClick()
+            } else {
+                // alert('press')
+                if (props.onLongPress) props.onLongPress()
+            }
+        }
+    }
+
+    function handleTouchMove() {
+        isTouch = false
+    }
+
+    // function handleClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    //     e.preventDefault()
+    //   }
+
     return (
-        <div onClick={props.onClick}>
+        <div onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd} onTouchMove={handleTouchMove} onMouseDown={handleTouchStart} onMouseUp={handleTouchEnd}>
             <div className="item-root">
                 <img className="item-icon" src={selectImg(props.icon)} alt={props.icon} />
                 <div className="item-text">
@@ -34,7 +64,7 @@ function Item(props: Props) {
                     <br />
                     <span className="item-description">{props.description}</span>
                 </div>
-                {props.isSelect?<img className="item-select" src={selectPng} alt="select" />:null}
+                {props.isSelect ? <img className="item-select" src={selectPng} alt="select" /> : null}
             </div>
         </div>
     )
