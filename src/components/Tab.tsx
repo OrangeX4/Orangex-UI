ed3e1048613459fd60b4f5dd353e1664971508b7
OrangeX4 <318483724@qq.com>
2020-10-07 14:26:13 +0800

    Update css

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -15,12 +15,15 @@ function Tab() {
         <div className='tab'>
             <div className='tab-pane'>
                 <img src={finuxActiveImg} className='tab-img' alt='file'></img>
+                <span className='tab-text-active'>File</span>
             </div>
             <div className='tab-pane'>
                 <img src={editImg} className='tab-img' alt='edit'></img>
+                <span className='tab-text'>Edit</span>
             </div>
             <div className='tab-pane'>
                 <img src={termImg} className='tab-img' alt='terminal'></img>
+                <span className='tab-text'>Terminal</span>
             </div>
         </div>
     )
