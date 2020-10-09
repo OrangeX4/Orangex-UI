6f98451fa5f773d16b650ef6e80a6a0d46e89300
OrangeX4 <318483724@qq.com>
2020-10-10 00:23:17 +0800

    Add terminal view

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import { Divider } from 'antd';
+
+import '../css/Terminal.css'
+
 interface Props {
 
 }
@@ -7,8 +11,33 @@ interface Props {
 function Terminal(props: Props) {
 
     return (
-        <div>
-            Terminal
+        <div className='terminal'>
+            <div className='terminal-out'>Out:</div>
+
+            <Divider>Current</Divider>
+            <div className='terminal-item'>
+                <div className='terminal-text'>
+                    <div className='terminal-text-title'>Title</div>
+                    <div className='terminal-text-content'>Content</div>
+                </div>
+                <div className='terminal-buttoncontainer'>
+                    <a className='terminal-button'>Button</a>
+                    <a className='terminal-button'>Button</a>
+                </div>
+            </div>
+            <div className='terminal-item'>Content</div>
+            <div className='terminal-item'>Content</div>
+
+            <Divider>File</Divider>
+            <div className='terminal-item'>Content</div>
+            <div className='terminal-item'>Content</div>
+            <div className='terminal-item'>Content</div>
+
+            <Divider>Default</Divider>
+            <div className='terminal-item'>Content</div>
+            <div className='terminal-item'>Content</div>
+            <div className='terminal-item'>Content</div>
+
         </div>
     )
 }
