53f5c087a25b6e72b0384afcc397a86aa6868520
OrangeX4 <318483724@qq.com>
2020-10-10 13:18:57 +0800

    Add Terminal Item event

--- src/components/TerminalItem.tsx
+++ src/components/TerminalItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 interface Props {
     title: string
@@ -7,14 +7,48 @@ interface Props {
 
 function TerminalItem(props: Props) {
 
+    // Click and Press
+    const [touchTimeOut, setTouchTimeOut] = useState(setTimeout(()=>{},0))
+    const [isClick, setIsClick] = useState(true)
+
+    function handleTouchStart() {
+        clearTimeout(touchTimeOut)
+        setTouchTimeOut(setTimeout(() => {
+            setIsClick(false)
+
+            // Long Press
+            alert('Press')
+        }, 500))
+    }
+
+    function handleTouchEnd() {
+        clearTimeout(touchTimeOut)
+        if (isClick) {
+            // Click
+            alert('Click')
+        } else {
+            setIsClick(true)
+        }
+    }
+
+    function handleTouchMove() {
+        clearTimeout(touchTimeOut)
+        setIsClick(false)
+    }
+
+    function handleRun(e: React.TouchEvent<HTMLAnchorElement>|React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        e.stopPropagation()
+        alert('Run')
+    }
+
     return (
-        <div className='terminal-item'>
+        <div className='terminal-item'  onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd} onTouchMove={handleTouchMove} onMouseDown={handleTouchStart} onMouseUp={handleTouchEnd}>
             <div className='terminal-text'>
                 <div className='terminal-text-title'>{props.title}</div>
                 <div className='terminal-text-content'>{props.content}</div>
             </div>
             <div className='terminal-buttoncontainer'>
-                <a className='terminal-button' href='/#'>Run</a>
+                <a onMouseDown={(e) => e.stopPropagation()} onTouchStart={(e) => e.stopPropagation()} onTouchEnd={handleRun} onMouseUp={handleRun} className='terminal-button' href='/#'>Run</a>
             </div>
         </div>
     )
